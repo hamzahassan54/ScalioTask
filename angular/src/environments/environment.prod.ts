035a606d12194ac42fd66d5484d6a82a977b57ec
hamzahassan54 <documentarytvofficial007@gmail.com>
2022-05-19 14:44:36 +0500

    set URL

--- angular/src/environments/environment.prod.ts
+++ angular/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   http_timeout: 150, //unit: seconds, 0: no timeout
-  apiUrl: "https://scalio-task.herokuapp.com/"
+  apiUrl: "https://scalio-task.herokuapp.com"
 };
